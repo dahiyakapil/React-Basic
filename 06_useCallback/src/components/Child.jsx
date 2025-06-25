6f2a1b391b8515021bd522d023c319daf7734cf8
KAPIL DAHIYA <kapildahiya309@gmail.com>
2025-06-25 11:51:54 +0530

    useCallback hook

--- 06_useCallback/src/components/Child.jsx
+++ 06_useCallback/src/components/Child.jsx
@@ -1,12 +1,25 @@
+// import React from 'react'
+
+// const Child = ({ handleClick }) => {
+//     console.log("Child Component re renders")
+//     return (
+//         <>
+//             <button onClick={handleClick}>Click Child button</button>
+//         </>
+//     )
+// }
+
+// export default React.memo(Child)
+
 import React from 'react'
 
-const Child = ({ handleClick }) => {
-    console.log("Child Component re renders")
-    return (
-        <>
-            <button onClick={handleClick}>Click Child button</button>
-        </>
-    )
+const Child = ({handleClick}) => {
+    console.log("Child Component rendered")
+  return (
+    <div>
+      <button onClick={handleClick}>Click Child Button</button>
+    </div>
+  )
 }
 
 export default React.memo(Child)
--- 06_useCallback/src/components/Parent.jsx
+++ 06_useCallback/src/components/Parent.jsx
@@ -1,29 +1,54 @@
-import React from 'react'
-import { useState } from 'react'
+// import React from 'react'
+// import { useState } from 'react'
+// import Child from './Child';
+// import { useCallback } from 'react';
+
+// const Parent = () => {
+//     const [count, setCount] = useState(0);
+//     const [subtract, setSubtract] = useState(100)
+
+//     function incrementCount() {
+//         setCount((prev) => prev + 1)
+//     }
+
+//     function decrement(){
+//         setSubtract((prev) => prev - 1)
+//     }
+
+//     const handleClick = useCallback(() => {
+//         console.log("Button Clicked");
+//     }, [subtract])
+//     return (
+//         <>
+//             <button onClick={incrementCount}>Increment Count</button> {count} <br />
+//             <button onClick={decrement}>Decrement Count</button> {subtract} <br />
+//             <Child handleClick={handleClick} />
+//         </>
+//     )
+// }
+
+// export default Parent
+
+
+
+import React, { useCallback, useState } from 'react'
 import Child from './Child';
-import { useCallback } from 'react';
 
 const Parent = () => {
     const [count, setCount] = useState(0);
-    const [subtract, setSubtract] = useState(100)
+    const [decrease, setDecrease] = useState(100);
 
-    function incrementCount() {
-        setCount((prev) => prev + 1)
-    }
-
-    function decrement(){
-        setSubtract((prev) => prev - 1)
-    }
 
     const handleClick = useCallback(() => {
         console.log("Button Clicked");
-    }, [subtract])
+    }, [decrease])
     return (
-        <>
-            <button onClick={incrementCount}>Increment Count</button> {count} <br />
-            <button onClick={decrement}>Decrement Count</button> {subtract} <br />
+        <div>
+
+            <button onClick={() => setCount(count + 1)}>Increment Count</button> {count}
             <Child handleClick={handleClick} />
-        </>
+            <button onClick={() => setDecrease(decrease - 1)}>Decrement</button> {decrease}
+        </div>
     )
 }
 
